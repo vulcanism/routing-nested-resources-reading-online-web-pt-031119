531aab1e9e74ca90c296067aaa4937400456dba8
Savannah C <sgcorrell@gmail.com>
2019-08-14 16:11:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :authors, only: [:show] do
     # nested resource for posts
     resources :posts, only: [:show, :index]
